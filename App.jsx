5431b008deee79a0f5d745f7ffc99c7b424e9c6d
SACHINSHRICHAND <sachinchoudhary88190@gmail.com>
2024-08-04 11:07:51 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import Calculator from './components/Calculator'
+
+function App() {
+ 
+  return (
+    
+       <Calculator></Calculator>
+  )
+}
+
+export default App
